3ce748f69c1e7f58113e91be0a153fbf57b2637f
roshmaep <roshmaep1999@gmail.com>
2022-11-21 10:32:48 +0530

    addition

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int c;
+        int c,a,b,x;
         Scanner sc=new Scanner(System.in);
    do {
        System.out.println("Main menu\n1.Addition\n2.subtraction\n3.multiptication\n4.Division");
        System.out.println("Enter your choice");
        c=sc.nextInt();
+       switch(c){
+           case 1:System.out.println("Enter 2 number");
+           a=sc.nextInt();
+           b= sc.nextInt();
+            x=a+b;
+               System.out.println(a+"+"+b+"="+x);
+       }
 
 
 
-   }while(C!=4);
+   }while(c!=4);
 
     }
 }
