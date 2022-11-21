0220534203557c0bd362ce79df97bb2a34157da3
roshmaep <roshmaep1999@gmail.com>
2022-11-21 10:24:09 +0530

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int c;
+        Scanner sc=new Scanner(System.in);
+   do {
+       System.out.println("Main menu\n1.Addition\n2.subtraction\n3.multiptication\n4.Division");
+       System.out.println("Enter your choice");
+       c=sc.nextInt();
 
+
+
+   }while(C!=4);
 
     }
 }
